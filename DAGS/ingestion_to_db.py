5079bf693a305b982705401266c8f1c33e5956c5
Marina Estefania <45777480+MarinaEstefania@users.noreply.github.com>
2022-07-12 18:31:25 -0500

    Update ingestion_to_db.py

--- DAGS/ingestion_to_db.py
+++ DAGS/ingestion_to_db.py
@@ -1,11 +1,12 @@
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 
-with DAG("db_ingestion", start_date=days_ago(1), schedule_interval='@once') as dag:
+with DAG("db_ingestion", start_date=days_ago(1)) as dag:
     start_workflow = DummyOperator(task_id="start_worklow")
     validate = DummyOperator(task_id="validate")
-    prepare = DummyOperator(task_id="prepare")
+    prepare = DummyOperator(task_id="prepare", postgres_conn_id="", sql="")
     load = DummyOperator(task_id="load")
     end_workflow = DummyOperator(task_id="start_worklow")
 
