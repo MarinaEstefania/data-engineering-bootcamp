d573a5ea5c484ecefca6f563224529eda02e2237
MarinaEstefania <m.garciac11@gmail.com>
2022-07-15 20:12:55 -0500

    create 3 s3 buckets

--- terraforms-templates/aws/modules/s3/main.tf
+++ terraforms-templates/aws/modules/s3/main.tf
@@ -9,3 +9,24 @@ resource "aws_s3_bucket" "bucket" {
     Name = "s3-data-bootcamp"
   }
 }
+
+resource "aws_s3_bucket_object" "object_movie" {
+  bucket   = aws_s3_bucket.bucket.id
+  key      = "movie_reviews.csv"
+  source   = "data/movie_review.csv"
+  etag     = filemd5("data/movie_review.csv")
+}
+
+resource "aws_s3_bucket_object" "object_log" {
+  bucket   = aws_s3_bucket.bucket.id
+  key      = "log_reviews.csv"
+  source   = "DATA/log_reviews.csv"
+  etag     = filemd5("DATA/log_reviews.csv")
+}
+
+resource "aws_s3_bucket_object" "object_purchase" {
+  bucket   = aws_s3_bucket.bucket.id
+  key      = "user_purchase.csv"
+  source   = "DATA/user_purchase.csv"
+  etag     = filemd5("DATA/user_purchase.csv")
+}
