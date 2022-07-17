840f5a808ce61ad7b62db08aecca132b670bc488
MarinaEstefania <m.garciac11@gmail.com>
2022-07-17 02:37:22 -0500

    adding os find path

--- DAGS/local_to_s3.py
+++ DAGS/local_to_s3.py
@@ -1,12 +1,30 @@
+from fileinput import filename
 import os
 
 from airflow import models
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.utils.dates import datetime
 from airflow.utils.dates import days_ago
+from airflow.hooks.postgres_hook import PostgresHook
 
 S3_BUCKET = os.environ.get("S3_BUCKET", "test-bucket")
 S3_KEY = os.environ.get("S3_KEY", "key")
+
+file=""
+def find_path():
+    # CSV loading to table.
+    # Getting the current work directory (cwd)
+    table_dir = os.getcwd()
+    print ("table dir = " + table_dir)
+    # r=root, d=directories, f=files
+    for r, d, f in os.walk(table_dir):
+        for file in f:
+            if file.endswith("movie_review.csv"):
+                table_path = os.path.join(r, file)
+
+    print("table path = " + table_path)
+
+    file = table_path
 
 with models.DAG(
     "upload_local_to_s3",
@@ -16,7 +34,7 @@ with models.DAG(
 ) as dag:
     create_local_to_s3_job = LocalFilesystemToS3Operator(
         task_id="create_local_to_s3_job",
-        filename="C:/Users/mgarc/Documents/wizeline/bootcamp/data-engineering-bootcamp/DATA/movie_review.csv",
+        filename=file,
         dest_key=S3_KEY,
         dest_bucket=S3_BUCKET,
         replace=True,
