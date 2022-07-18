ac79d20183030ba807b8fec147b982bb53732bf0
MarinaEstefania <m.garciac11@gmail.com>
2022-07-17 23:03:59 -0500

    getting connection values

--- DAGS/csv_to_pg_v2.py
+++ DAGS/csv_to_pg_v2.py
@@ -21,16 +21,17 @@ def upload_data_func():
     #    print('{}: {}'.format(item, value))
     S3_BUCKET = Variable.get("S3_BUCKET")
     logging.info(S3_BUCKET)
-    S3_KEY = os.environ.get("S3_KEY")
+    S3_KEY = Variable.get("S3_KEY")
     logging.info(S3_KEY)
-    s3_conn = os.environ.get("s3_conn")
+    s3_conn: str = "s3_conn"
     logging.info(s3_conn)
-    pg_conn = os.environ.get("pg_conn")
+    pg_conn: str = "pg_conn"
     logging.info(pg_conn)
+
     s3_hook = S3Hook(s3_conn)
     logging("you're about to call s3_hook.downoad_file")
-
     local_filename = s3_hook.download_file(key=S3_KEY, bucket_name=S3_BUCKET)
+    
     psql_hook = PostgresHook(pg_conn)
     psql_hook.copy_expert(sql = """COPY deb.user_purchase(
                 invoice_number,
