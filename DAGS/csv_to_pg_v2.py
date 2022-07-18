66216684e9e7c0a641315efb87f21f06063deecd
MarinaEstefania <m.garciac11@gmail.com>
2022-07-17 22:05:46 -0500

    adding upload_data first try

--- DAGS/csv_to_pg_v2.py
+++ DAGS/csv_to_pg_v2.py
@@ -1,13 +1,36 @@
 from asyncio import tasks
 import logging
+import os
+from ssl import SSLSocket
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.utils.dates import days_ago
 
 DAG_ID = "csv_to_pg_v2"
+
+
+S3_BUCKET = os.environ.get("S3_BUCKET", "test-bucket")
+S3_KEY = os.environ.get("S3_KEY", "key")
+
+def upload_data_func():
+    s3_hook = S3Hook(s3_conn)
+    local_filename = s3_hook.download_file(key=S3_KEY, bucket_name=S3_BUCKET)
+    psql_hook = PostgresHook(pg_conn)
+    psql_hook.copy_expert(sql = """COPY user_purchase(
+                invoice_number,
+                stock_code,
+                detail,
+                quantity,
+                invoice_date,
+                unit_price,
+                customer_id,
+                country) 
+                FROM STDIN
+                DELIMITER ',' CSV HEADER;""", filename = local_filename)
 
 def get_table_count():
     pg_hook = PostgresHook(postgres_conn_id='pg_conn')
@@ -17,7 +40,7 @@ def get_table_count():
     #cursor.close()
     #pg_conn.close
     logging.info("returning count: ")
-    cursor.fetchone()
+    logging.info(cursor.fetchone())
 
 
 with DAG(
@@ -48,17 +71,14 @@ with DAG(
         task_id = "count",
         python_callable = get_table_count
     )
-    load = PostgresOperator(
-        task_id="load",
-        postgres_conn_id="pg_conn", 
-        sql="""
-            \copy deb.user_purchase(InvoiceNo,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country)
-                FROM 'C:\\Users\\mgarc\\Documents\\wizeline\\bootcamp\\data-engineering-bootcamp\\DATA\\user_purchase.csv'
-                DELIMITER ','
-                CSV HEADER;
-            SELECT COUNT(*) AS total_rows FROM deb.user_purchase;
-            """
+    upload_data = PythonOperator(
+        task_id="upload_data",
+        python_callable = upload_data_func
+    )
+    count_after_populate = PythonOperator(
+        task_id = "count_after_populate",
+        python_callable = get_table_count
     )
     end_workflow = DummyOperator(task_id="end_worklow")
 
-    start_workflow >>  create_table >> count >> load >> end_workflow
+    start_workflow >>  create_table >> count >> upload_data >> count_after_populate >> end_workflow
